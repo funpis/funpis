fb0ce80c73888696cc1ac6da7f4305b51e13bd4c
funpis <liupengtuxio@gmail.com>
2017-04-14 19:33:22 +0900

    update chart

--- public/javascripts/vote.js
+++ public/javascripts/vote.js
@@ -142,43 +142,47 @@ function loadVoteGraph(div_topic) {
 
     div_topic.append(myCanvas);
 
-    var myChart = new Chart(myCanvas, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                /*
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                */
-                borderWidth: 1
+    var chart_type = "bar";
+
+    var chart_data = {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    var chart_options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
             }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
         }
+    };
+
+    var myChart = new Chart(myCanvas, {
+        type: chart_type,
+        data: chart_data,
+        options: chart_options
     });
 
     myCanvas.click(
@@ -190,6 +194,8 @@ function loadVoteGraph(div_topic) {
             alert(label);
             var value = myChart.data.datasets[0].data[clickedElementIndex];
             alert(value);
+            myChart.data.datasets[0].data[clickedElementIndex] = 18;
+            myChart.update();
         }
     );
 }
